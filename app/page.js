8b8436c3df99031cb1f282129e3758921c94f435
dom <dominicjottaviano@gmail.com>
2025-02-17 18:14:59 -0500

    basic stuff..

--- app/page.js
+++ app/page.js
@@ -1,95 +1,103 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import Image from 'next/image';
+import { Share_Tech_Mono } from 'next/font/google';
+import { useState, useEffect, useRef } from 'react';
+import styles from './page.module.css';
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+const board_width = 15;
+const board_height = 10;
+
+const boardToString = (board) => {
+	let out = '';
+	for(let i=0; i<board_height-1; i++) {
+		for(let j=0; j<board_width; j++)
+			out += board[i][j];
+		out += '\n';
+	}
+	for(let i=0; i<board_width; i++)
+		out += board[board_height-1][i];
+
+	return out;
 }
+
+const validPosition = (x, y) => {
+	return x >= 0 && x < board_width && y >= 0 && y < board_height;
+}
+
+const Home = () => {
+
+	const emptyBoard = () => {
+		const newBoard = Array(board_height).fill().map(() => Array(board_width).fill(' '));
+		newBoard[position.y][position.x] = '*';
+		return newBoard;
+	}
+
+	const [ displayString, setDisplayString ] = useState('');
+	const [ position, setPosition ] = useState({ x: 0, y: 0 });
+	const [ board, setBoard ] = useState(emptyBoard());
+	const previousPositionRef = useRef(position);
+
+	useEffect(() => {
+
+		const move = (xoff, yoff) => {
+			setPosition(prev => {
+				previousPositionRef.current = prev;
+				return {
+					x: Math.min(board_width-1, Math.max(prev.x + xoff, 0)), 
+					y: Math.min(board_height-1, Math.max(prev.y + yoff, 0)) 
+				}
+			});
+		}
+
+		const handleKeyDown = (e) => {
+			switch(e.key) {
+				case 'h':
+					move(-1, 0);
+					break;
+				case 'j':
+					move(0, 1);
+					break;
+				case 'k':
+					move(0, -1);
+					break;
+				case 'l':
+					move(1, 0);
+					break;
+			}
+		}
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			window.removeEventListeneer('keydown', handleKeyDown);
+		};
+	}, []);
+
+	useEffect(() => {
+		const prev = previousPositionRef.current;
+		setBoard(prev => {
+			const newBoard = prev.map(row => Array(board_width).fill(' '));
+			newBoard[position.y][position.x] = '*';
+			return newBoard;
+		});
+		console.log('Position: ', position);
+	}, [position]);
+
+	useEffect(() => {
+		setDisplayString(boardToString(board));
+	}, [board]);
+
+	useEffect(() => {
+		//console.log(displayString);
+	}, [displayString]);
+
+
+	return (
+		<div className={styles.page}>
+			<pre className={styles.container}>
+				{displayString}
+			</pre>
+		</div>
+	);
+}
+export default Home;
