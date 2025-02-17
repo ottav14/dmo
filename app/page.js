9452c80b530ae9fe1ce826601622a5474296e886
dom <dominicjottaviano@gmail.com>
2025-02-17 18:49:49 -0500

    Added a room.

--- app/page.js
+++ app/page.js
@@ -8,45 +8,78 @@ import styles from './page.module.css';
 const board_width = 15;
 const board_height = 10;
 
-const boardToString = (board) => {
-	let out = '';
-	for(let i=0; i<board_height-1; i++) {
-		for(let j=0; j<board_width; j++)
-			out += board[i][j];
-		out += '\n';
-	}
-	for(let i=0; i<board_width; i++)
-		out += board[board_height-1][i];
+const player_ch = '@';
 
-	return out;
-}
-
-const validPosition = (x, y) => {
-	return x >= 0 && x < board_width && y >= 0 && y < board_height;
-}
 
 const Home = () => {
 
-	const emptyBoard = () => {
+	const drawHLine = (x0, x1, y, board) => {
+		for(let i=x0; i<=x1; i++)
+			board[y][i] = '-';
+	}
+
+	const drawVLine = (y0, y1, x, board) => {
+		for(let i=y0; i<=y1; i++)
+			board[i][x] = '|';
+	}
+
+	const initBoard = () => {
 		const newBoard = Array(board_height).fill().map(() => Array(board_width).fill(' '));
-		newBoard[position.y][position.x] = '*';
+
+		drawVLine(3, 9, 3, newBoard);
+		drawVLine(3, 9, 9, newBoard);
+		drawHLine(3, 9, 3, newBoard);
+		drawHLine(3, 9, 9, newBoard);
+
+		newBoard[3][6] = ' ';
+
 		return newBoard;
 	}
 
 	const [ displayString, setDisplayString ] = useState('');
 	const [ position, setPosition ] = useState({ x: 0, y: 0 });
-	const [ board, setBoard ] = useState(emptyBoard());
+	const [ board, setBoard ] = useState(initBoard());
 	const previousPositionRef = useRef(position);
+
+
+	const boardToString = (board) => {
+
+		// Clone board
+		const _board = board.map(row => [...row]);
+
+		// Add player
+		_board[position.y][position.x] = player_ch;
+
+		let out = '';
+		for(let i=0; i<board_height-1; i++) {
+			for(let j=0; j<board_width; j++)
+				out += _board[i][j];
+			out += '\n';
+		}
+		for(let i=0; i<board_width; i++)
+			out += _board[board_height-1][i];
+
+		return out;
+	}
+
+	const validPosition = (x, y) => {
+		const inBounds = x >= 0 && x < board_width && y >= 0 && y < board_height;
+		const isAir = inBounds && board[y][x] === ' ';
+		return inBounds && isAir;
+	}
 
 	useEffect(() => {
 
 		const move = (xoff, yoff) => {
 			setPosition(prev => {
 				previousPositionRef.current = prev;
-				return {
-					x: Math.min(board_width-1, Math.max(prev.x + xoff, 0)), 
-					y: Math.min(board_height-1, Math.max(prev.y + yoff, 0)) 
+				if(validPosition(prev.x+xoff, prev.y+yoff)) {
+					return {
+						x: prev.x + xoff, 
+						y: prev.y + yoff 
+					}
 				}
+				return prev;
 			});
 		}
 
@@ -74,17 +107,10 @@ const Home = () => {
 	}, []);
 
 	useEffect(() => {
-		const prev = previousPositionRef.current;
-		setBoard(prev => {
-			const newBoard = prev.map(row => Array(board_width).fill(' '));
-			newBoard[position.y][position.x] = '*';
-			return newBoard;
-		});
-		console.log('Position: ', position);
+		setDisplayString(boardToString(board));
 	}, [position]);
 
 	useEffect(() => {
-		setDisplayString(boardToString(board));
 	}, [board]);
 
 	useEffect(() => {
