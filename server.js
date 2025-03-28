a4190640203d20bc94b62ff8fd0bce77a8cb090c
ottav14 <dominicjottaviano@gmail.com>
2025-03-28 04:50:36 +0000

    https stuff i think.

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const https = require('https');
+const fs = require('fs');
+
+const PORT = 443;
+const options = {
+	  key: fs.readFileSync("/etc/letsencrypt/live/ottavhq.com/privkey.pem"),
+	  cert: fs.readFileSync("/etc/letsencrypt/live/ottavhq.com/fullchain.pem"),
+};
 
 app.use(express.static('public'));
 
@@ -9,7 +17,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+https.createServer(options, app).listen(PORT, () => {
+	  console.log(`Server running at https://ottavhq.com`);
 });
-
