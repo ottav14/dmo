c8f301cb6da05e833afe767539d1e7b31e58a380
ottav14 <dominicjottaviano@gmail.com>
2025-04-10 01:17:03 +0000

    Stable with stuff.

--- public/controls.js
+++ public/controls.js
@@ -20,6 +20,16 @@ const changeBoard = (state, xoff, yoff, webSocketServer) => {
 	webSocketServer.send(JSON.stringify(positionMessage));
 }
 
+const getChar = (direction) => {
+	switch(direction) {
+		case 'up':    return '^';
+		case 'down':  return 'v';
+		case 'left':  return '<';
+		case 'right': return '>';
+	}
+	return null;
+}
+
 export const move = (state, xoff, yoff, webSocketServer) => {
 	const p = state.position;
 	const newP = { 
@@ -33,12 +43,17 @@ export const move = (state, xoff, yoff, webSocketServer) => {
 	];
 
 	const directionKey = {
-		'0,-1': '^',
-		'0,1': 'v',
-		'-1,0': '<',
-		'1,0': '>',
+		'0,-1': 'up',
+		'0,1':  'down',
+		'-1,0': 'left',
+		'1,0':  'right',
 	}
-	const ch = directionKey[`${xoff},${yoff}`];
+	const direction = directionKey[`${xoff},${yoff}`];
+	state.direction = direction;
+	const ch = getChar(state.direction);
+
+	if(state.mode !== 'normal')
+		state.player_ch = ch;
 
 	const bx = state.boardPosition.x;
 	const by = state.boardPosition.y;
@@ -50,18 +65,50 @@ export const move = (state, xoff, yoff, webSocketServer) => {
 	   		newP.y >= 0 && newP.y < PARAMS.board_height &&
 	   !unwalkables.includes(state.board[newP.y][newP.x])) {
 		state.position = newP;
-		if(state.mode === 'building')
-			state.player_ch = ch;
 		const message = { type: 'position', id: state.id, data: newP };
 		webSocketServer.send(JSON.stringify(message));
 	}
 }
 
 const placeBlock = (e, state, webSocketServer) => {
-	state.board[state.position.y][state.position.x] = state.block;
-	const edit = { position: state.position, boardPosition: state.boardPosition, ch: state.block };
-	const message = { type: 'block', id: state.id, data: edit };
-	webSocketServer.send(JSON.stringify(message));
+	const directionKey = {
+		'up': [0, -1],
+		'down': [0, 1],
+		'left': [-1, 0],
+		'right': [1, 0]
+	}
+	const x = state.position.x + directionKey[state.direction][0];
+	const y = state.position.y + directionKey[state.direction][1];
+	const w = PARAMS.board_width;
+	const h = PARAMS.board_height;
+	if(x >= 0 && x < w && y >= 0 && y < h) {
+		state.board[y][x] = state.block;
+		const edit = { position: { x: x, y: y }, boardPosition: state.boardPosition, ch: state.block };
+		const message = { type: 'block', id: state.id, data: edit };
+		webSocketServer.send(JSON.stringify(message));
+	}
+}
+
+const shoot = (e, state, webSocketServer) => {
+
+	const directionKey = {
+		'up': [0, -1],
+		'down': [0, 1],
+		'left': [-1, 0],
+		'right': [1, 0]
+	}
+	const x = state.position.x + directionKey[state.direction][0];
+	const y = state.position.y + directionKey[state.direction][1];
+	const w = PARAMS.board_width;
+	const h = PARAMS.board_height;
+	if(x >= 0 && x < w && y >= 0 && y < h) {
+		state.board[y][x] = 'o';
+		const edit = { position: { x: x, y: y }, boardPosition: state.boardPosition, ch: state.block };
+		const message = { type: 'block', id: state.id, data: edit };
+		webSocketServer.send(JSON.stringify(message));
+	}
+	
+
 }
 
 const normalControls = (e, state, webSocketServer) => {
@@ -92,7 +139,12 @@ const normalControls = (e, state, webSocketServer) => {
 			break;
 		case 'b':
 			state.mode = 'building';
-			state.player_ch = '>';
+			state.player_ch = getChar(state.direction);
+			console.log(state.player_ch);
+			break;
+		case 'g':
+			state.mode = 'shooting';
+			state.player_ch = getChar(state.direction);
 			break;
 	}
 }
@@ -163,6 +215,9 @@ const buildingControls = (e, state, webSocketServer) => {
 		case 'p':
 			state.mode = 'pick';
 			break;
+		case 'g':
+			state.mode = 'shooting';
+			break;
 		case 'Escape':
 			state.mode = 'normal';
 			state.player_ch = '@';
@@ -185,6 +240,45 @@ const pickControls = (e, state, webSocketServer) => {
 	
 }
 
+const shootingControls = (e, state, webSocketServer) => {
+	switch(e.key) {
+		case 'ArrowLeft':
+		case 'a':
+		case 'h':
+			move(state, -1, 0, webSocketServer);
+			break;
+		case 'ArrowDown':
+		case 's':
+		case 'j':
+			move(state, 0, 1, webSocketServer);
+			break;
+		case 'ArrowUp':
+		case 'w':
+		case 'k':
+			move(state, 0, -1, webSocketServer);
+			break;
+		case 'ArrowRight':
+		case 'd':
+		case 'l':
+			move(state, 1, 0, webSocketServer);
+			break;
+		case ' ':
+			shoot(e, state, webSocketServer);
+			break;
+		case 'b':
+			state.mode = 'building';
+			break;
+		case ':':
+			state.prevMode = state.mode;
+			state.mode = 'typing';
+			break;
+		case 'Escape':
+			state.mode = 'normal';
+			state.player_ch = '@';
+			break;
+	}
+}
+
 export const controls = (e, state, webSocketServer) => {
 
 	switch(state.mode) {
@@ -199,6 +293,9 @@ export const controls = (e, state, webSocketServer) => {
 			break;
 		case 'pick':
 			pickControls(e, state, webSocketServer);
+			break;
+		case 'shooting':
+			shootingControls(e, state, webSocketServer);
 			break;
 		case 'loading':
 			break;
--- public/render.js
+++ public/render.js
@@ -1,5 +1,55 @@
 import * as PARAMS from './params.js';
 
+const addGUI = (board) => {
+
+	const width = PARAMS.display_width;
+	const height = PARAMS.display_height;
+
+	const display = Array(height).fill().map((val, i) => {
+		if(i < PARAMS.board_height)
+			return [...board[i]];
+		else
+			return Array(width).fill(' ');
+	});
+
+	drawHLine(0, PARAMS.board_width-1, PARAMS.board_height, display);
+	drawVLine(0, height-1, PARAMS.board_width, display);
+
+	return display;
+
+}
+
+const addMessages = (board, activeMessages) => {
+	for(let i=activeMessages.length-1; i>=0; i--) {
+		const x = PARAMS.board_width+2;
+		const y = 2*(activeMessages.length - i) - 1;
+
+		const name = activeMessages[i].name;
+		for(let j=0; j<name.length; j++)
+			board[y][x+j] = name[j];
+
+		board[y][x+name.length] = ':';
+
+		const msg = activeMessages[i].text;
+		for(let j=0; j<msg.length; j++)
+			board[y][x+name.length+j+2] = msg[j];
+	}
+}
+
+const addChatInput = (board, message) => {
+	const y = PARAMS.display_height-2;
+	board[y][1] = ':';
+	for(let i=0; i<message.length; i++)
+		board[y][i+3] = message[i];
+}
+
+const addModeDisplay = (board, mode) => {
+	const y = PARAMS.display_height-2;
+	const x = PARAMS.board_width-10;
+	for(let i=0; i<mode.length; i++)
+		board[y][x+i] = mode[i];
+}
+
 export const updateBoard = (state) => {
 
 	if(state.board.length < 1)
@@ -31,41 +81,33 @@ export const updateBoard = (state) => {
 		}
 	}
 
-	// Add message being typed
-	if(mode === 'typing') {
-		_board[PARAMS.board_height-1][0] = ':';
-		for(let i=0; i<message.length; i++)
-			_board[PARAMS.board_height-1][i+1] = message[i];
-	}
 
-	// Add typed messages
-	for(let i=activeMessages.length-1; i>=0; i--) {
-		const x = PARAMS.border_width+2;
-		const y = 2*(activeMessages.length - i) - 1;
-
-		const name = activeMessages[i].name;
-		for(let j=0; j<name.length; j++)
-			_board[y][x+j] = name[j];
-
-		_board[y][x+name.length] = ':';
-
-		const msg = activeMessages[i].text;
-		for(let j=0; j<msg.length; j++)
-			_board[y][x+name.length+j+2] = msg[j];
-	}
 
 	// Building block display
 	_board[PARAMS.board_height-2][PARAMS.board_width-2] = state.block;
 
+	// GUI 
+	const display = addGUI(_board);
+
+	// Mode display
+	addModeDisplay(display, state.mode);
+
+	// Message being typed
+	if(mode === 'typing')
+		addChatInput(display, state.message); 
+
+	// Typed messages
+	addMessages(display, state.activeMessages);
+
 	// Construct display string
 	let out = '';
-	for(let i=0; i<PARAMS.board_height-1; i++) {
-		for(let j=0; j<PARAMS.board_width; j++)
-			out += _board[i][j];
+	for(let i=0; i<PARAMS.display_height-1; i++) {
+		for(let j=0; j<PARAMS.display_width; j++)
+			out += display[i][j];
 		out += '\n';
 	}
-	for(let i=0; i<PARAMS.board_width; i++)
-		out += _board[PARAMS.board_height-1][i];
+	for(let i=0; i<PARAMS.display_width; i++)
+		out += display[PARAMS.display_height-1][i];
 
 	state.container.innerText = out;
 }
